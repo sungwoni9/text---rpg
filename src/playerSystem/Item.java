a080d1b53f1fcd9c49446c3616a2613ece215bab
sungwoni9 <hurdang99@naver.com>
2024-11-18 23:47:57 +0900

    Add constructor luck

--- src/playerSystem/Item.java
+++ src/playerSystem/Item.java
@@ -2,4 +2,12 @@ package playerSystem;
 
 public class Item {
 
+	public static Item weapon;
+	public static Item armor;
+	public static Item ring;
+
+	public int getPower() {
+		return null;
+	}
+
 }
