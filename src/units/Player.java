75040f1e5f9c2dc82f465930445b8e6abe384bbf
허성원 <hurdang99@naver.com>
2024-11-18 18:33:36 +0900

    Modify and Add get,set method

--- src/units/Player.java
+++ src/units/Player.java
@@ -8,28 +8,115 @@ import playerSystem.Item;
 
 public class Player extends Unit {
 
+	private String name;
+	private int level;
+	private int maxHp;
+	private int att;
+	private int def;
+	private int exp;
+	private boolean party;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getMaxHp() {
+		return maxHp;
+	}
+
+	public void setMaxHp(int maxHp) {
+		this.maxHp = maxHp;
+	}
+
+	public int getAtt() {
+		return att;
+	}
+
+	public void setAtt(int att) {
+		this.att = att;
+	}
+
+	public int getDef() {
+		return def;
+	}
+
+	public void setDef(int def) {
+		this.def = def;
+	}
+
+	public int getExp() {
+		return exp;
+	}
+
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+
+	public boolean isParty() {
+		return party;
+	}
+
+	public void setParty(boolean party) {
+		this.party = party;
+	}
+
+	public static int getMoney() {
+		return money;
+	}
+
+	public static void setMoney(int money) {
+		Player.money = money;
+	}
+
+	public static Inventory getInven() {
+		return inven;
+	}
+
+	public static void setInven(Inventory inven) {
+		Player.inven = inven;
+	}
+
+	public boolean isMember() {
+		return member;
+	}
+
+	public void setMember(boolean member) {
+		this.member = member;
+	}
+
 	public static int money;
 
-	static Guild guild = new Guild();
 	static Inventory inven = new Inventory();
 
 	public boolean member;
 
-	public Player(String name, int level, int maxhp, int maxMp, int att, int def, int exp) {
-		super(name, level, maxhp, maxMp, att, def, exp);
+	public Player(String name, int level, int maxHp, int maxMp, int att, int def, int exp) {
+		super(name, level, maxHp, maxMp, att, def, exp);
 	}
 
-	public Player(String name, int level, int maxhp, int att, int def, int exp, boolean party) {
-		super(name, level, maxhp, att, def, exp, false);
+	public Player(String name, int level, int maxHp, int att, int def, int exp, boolean party) {
+		super(name, level, maxHp, att, def, exp, false);
 	}
 
 	public Player() {
-		this("", 1, 100, 10, 5, 0, false);
+
 	}
 
 	void init() {
 		money = 10000;
-		guild.setGuild();
+		Guild.setGuild();
 
 	}
 
@@ -62,23 +149,19 @@ public class Player extends Unit {
 	}
 
 	public void printData() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	public int getCurrentHp() {
-		// TODO Auto-generated method stub
-		return 0;
+		this.currentHp = currentHp;
 	}
 
 	public String name() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void attack(Monster monster) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
