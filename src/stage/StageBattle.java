6021a875d6399af6419a45713884175a83cca25b
허성원 <hurdang99@naver.com>
2024-11-18 17:48:18 +0900

    Modify all method

--- src/stage/StageBattle.java
+++ src/stage/StageBattle.java
@@ -11,7 +11,7 @@ import units.UnitManager;
 public class StageBattle extends Stage {
 
 	UnitManager unit = null;
-	Vector<Monster> monList = null;
+	Vector<Monster> monsterList = null;
 	Random random;
 	int monsterDead = 0;
 	int playerDead = 0;
@@ -39,7 +39,7 @@ public class StageBattle extends Stage {
 					player_N = 0;
 				}
 			} else if (!turn) {
-				if (monster_N < monList.size()) {
+				if (monster_N < monsterList.size()) {
 					monster_attack(monster_N);
 					monster_N += 1;
 				} else {
@@ -61,55 +61,94 @@ public class StageBattle extends Stage {
 		unit.getMon_list().clear();
 		unit.monster_rand_set(4);
 		unit.Player = null;
-		unit.Player = new Player();
-		monList = null;
-		monList = unit.getMon_list();
-		monsterDead = monList.size();
+		monsterList = null;
+		monsterList = unit.getMon_list();
+		monsterDead = monsterList.size();
 		playerDead = Player.getGuildSize();
 
 	}
 
 	void print_character() {
 		buffer.setLength(0);
-		buffer.append("======[BATTLE]======");
-		buffer.append(playerDead + " : " + monsterDead);
-		buffer.append("======[PLAYER]======");
+		buffer.append("======[BATTLE]======\n");
+		buffer.append(playerDead + "\t : \t" + monsterDead);
+		buffer.append("\n======[PLAYER]======");
+
+		for (int i = 0; i < Player.getGuildSize(); i++) {
+			Player.getGuildUnit(i).printData();
+		}
+		buffer.append("\n======[MONSTER]======");
+		for (int i = 0; i < monsterList.size(); i++) {
+			monsterList.get(i).printData();
+		}
 
 		try {
 			writer.write(buffer.toString());
 			writer.flush();
 		} catch (Exception e) {
 		}
-		for (int i = 0; i < Player.getGuildSize(); i++) {
+	}
+
+	void player_attack(int index) {
+		Player player = Player.getGuildUnit(index);
+
+		if (player.getCurrentHp() <= 0)
+			return;
+
+		buffer.append("======[메뉴 선택]=====");
+		buffer.append("[" + player.name() + "] [1.어택] [2.스킬]");
+
+		try {
+			writer.write(buffer.toString());
+			writer.flush();
+
+			String input = reader.readLine();
+			int sel = Integer.parseInt(input);
+			if (sel == 1) {
+				while (true) {
+					int idx = random.nextInt(monsterList.size());
+					if (monsterList.get(idx).currentHp > 0) {
+						player.attack(monsterList.get(idx));
+						break;
+					}
+				}
+			} else if (sel == 2) {
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		System.out.println("======[MONSTER]======");
-		for (int i = 0; i < monList.size(); i++) {
+
+	}
+
+	void monster_attack(int index) {
+		Monster monster = monsterList.get(index);
+		if (monster.getCurrentHp() <= 0)
+			return;
+		while (true) {
+			int idx = random.nextInt(Player.getGuildSize());
+			if (monster.getCurrentHp() > 0) {
+				monster.attack(Player.getGuildUnit(idx));
+				break;
+			}
 		}
 	}
 
 	void check_live() {
 		int num = 0;
 		for (int i = 0; i < Player.getGuildSize(); i++) {
-			if (Player.getGuildUnit(i).hp <= 0) {
+			if (Player.getGuildUnit(i).getCurrentHp() <= 0) {
 				num += 1;
 			}
 		}
 		playerDead = Player.getGuildSize() - num;
 
-		num = 0;
-		for (int i = 0; i < monList.size(); i++) {
-			if (monList.get(i).currentHp <= 0) {
+		for (int i = 0; i < monsterList.size(); i++) {
+			if (monsterList.get(i).getCurrentHp() <= 0) {
 				num += 1;
 			}
 		}
-		monsterDead = monList.size() - num;
-	}
-
-	private void monster_attack(int m_index) {
-
-	}
-
-	private void player_attack(int p_index) {
-
+		monsterDead = monsterList.size() - num;
 	}
 }
