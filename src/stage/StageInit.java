785ab3b1fad507df13fbf88bd5b3387422e48827
허성원 <hurdang99@naver.com>
2024-11-19 17:14:27 +0900

    Modify : update improvement readability

--- src/stage/StageInit.java
+++ src/stage/StageInit.java
@@ -1,35 +1,59 @@
 package stage;
 
-import textRpg.GameManager;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
-public class StageInit extends Stage{
+import manager.IOManager;
+
+public class StageInit implements Stage {
+	StringBuilder bf = IOManager.buffer;
+	BufferedReader br = IOManager.reader;
+	BufferedWriter wr = IOManager.writer;
 
 	@Override
 	public boolean update() {
+		printMainMenuGuide();
 		try {
-			buffer.setLength(0);
+			String input = (String) IOManager.selMenu(String.class, "☞");
 			
-			buffer.append("\n==== TEXT RPG ====");
-			buffer.append("\n[시작] 을 입력하세요\n☞");
-			
-			writer.write(buffer.toString());
-			writer.flush();
+			if ("시작".equals(input)) {
+				StageSetting.nextStage = "LOBBY";
+				return true;
+			} else if ("종료".equals(input))
+				printExitMessage();
 
-			String input = reader.readLine();
-			if (input != null && input.equals("시작")) {
-				GameManager.nextStage = "LOBBY";
-				return false;
-			}
 		} catch (Exception e) {
 			System.out.println("다시 입력해주세요");
 
 		}
 		return false;
+
 	}
 
 	@Override
 	public void init() {
 
+	}
+
+	private void printMainMenuGuide() {
+		String msg = """
+				=========================
+				=         TEXT PG       =
+				=      시작하려면 "시작",    =
+				=      종료하려면 "종료"를    =
+				=         입력하세세요.     =
+				=========================
+				""";
+		IOManager.printString(msg);
+	}
+
+	private void printExitMessage() {
+		String msg = """
+				=======================
+				=   게임을 를 종료합니다    =
+				=======================
+				""";
+		IOManager.printString(msg);
 	}
 
 }
