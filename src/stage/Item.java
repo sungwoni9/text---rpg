f149a3e24cb28435ab7626d5d8c9e6a651c99716
허성원 <hurdang99@naver.com>
2024-11-19 18:30:21 +0900

    Create Item class

--- src/stage/Item.java
+++ src/stage/Item.java
@@ -1,13 +1,23 @@
-package playerSystem;
+package stage;
 
-public class Item {
+public class Item implements Stage {
 
 	public static Item weapon;
 	public static Item armor;
 	public static Item ring;
 
+	@Override
+	public boolean update() {
+		return false;
+	}
+
+	@Override
+	public void init() {
+
+	}
+
 	public int getPower() {
-		return null;
+		return 0;
 	}
 
 }
