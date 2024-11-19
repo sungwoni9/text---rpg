58836758b64fe2a9c4a2ad0a902ef617885dbf3b
허성원 <hurdang99@naver.com>
2024-11-19 14:47:31 +0900

    Modify : move method to GameManager

--- src/stage/StageSetting.java
+++ src/stage/StageSetting.java
@@ -1,5 +1,26 @@
 package stage;
 
-public class StageSetting {
+import java.util.Map;
+
+import units.UnitManager;
+
+public class StageSetting implements Stage {
+	private Map<String, Stage> stageList;
+	@Override
+	void init() {
+		UnitManager.getInstance();
+		stageList.put("TITLE", new StageInit());
+		stageList.put("LOBBY", new StageLobby());
+		stageList.put("MENU", new StageMenu());
+		stageList.put("GUILDMENU", new Guild());
+		stageList.put("STOREMENU", new Shop());
+		stageList.put("INVENMENU", new Inventory());
+		nextStage = "TITLE";
+	}
+
+	@Override
+	public void activateStage() {
+
+	}
 
 }
