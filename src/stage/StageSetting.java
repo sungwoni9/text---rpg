6e261a7ba1f9e9f2c866c40eaf03bd52f2611804
허성원 <hurdang99@naver.com>
2024-11-19 15:04:43 +0900

    Modify : activateStage and changeStage method

--- src/stage/StageSetting.java
+++ src/stage/StageSetting.java
@@ -1,15 +1,18 @@
 package stage;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import textRpg.GameManager;
 import units.UnitManager;
 
 public class StageSetting implements Stage {
-	private Map<String, Stage> stageList;
+	private Map<String, Stage> stageList =  new HashMap<>();
 
 	public static String nextStage = "";
 	public static String currentStage = "";
+
+	public static Object instance;
 
 	@Override
 	public void init() {
@@ -45,13 +48,39 @@ public class StageSetting implements Stage {
 		return currentStage;
 	}
 
+	public boolean changeStage() {
+		try {
+			GameManager.writer.write("스테이지 변경 중...\n");
+			GameManager.writer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (currentStage.equals(nextStage)) {
+			return true;
+		}
+
+		currentStage = nextStage;
+		Stage stage = stageList.get(currentStage);
+		if (stage != null) {
+			stage.init();
+			while (stage.activate()) {
+			}
+		} else {
+			System.err.println("잘못된 스테이지: " + currentStage);
+			return false;
+		}
+
+		return !nextStage.isEmpty();
+	}
+
 	@Override
 	public void activateStage() {
 		Stage stage = stageList.get(currentStage);
 		if (stage != null) {
 			stage.activate();
 		} else {
-			System.err.println("잘못된 스테이지 이름: " + currentStage);
+			System.err.println("잘못된 스테이지 : " + currentStage);
 		}
 	}
 
