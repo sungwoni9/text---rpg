9c976c81981f13d4610585aebb6aaf6e5cda189d
허성원 <hurdang99@naver.com>
2024-11-19 17:12:21 +0900

    Modify : add constructor and update

--- src/textRpg/TextRPG.java
+++ src/textRpg/TextRPG.java
@@ -1,5 +1,20 @@
 package textRpg;
 
-public class TextTPG {
+import manager.IOManager;
+import stage.StageSetting;
+
+public class TextRPG {
+
+	private StageSetting stageSetting;
+	private boolean isRun = false;
+
+	public TextRPG() {
+		stageSetting = new StageSetting();
+	}
+
+	
+	public static TextRPG getInstance() {
+		return getInstance();
+	}
 
 }
